9d2326ef06f247eb1a6e86590ffca41883fc85a9
Josh Morony <joshua.morony@gmail.com>
2018-09-19 11:18:14 +0930

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public myForm: FormGroup;
+  private playerCount: number = 1;
+
+  constructor(private formBuilder: FormBuilder){
+
+    this.myForm = formBuilder.group({
+      player1: ['', Validators.required]
+    });
+
+  }
+
+  addControl(){
+    this.playerCount++;
+    this.myForm.addControl('player' + this.playerCount, new FormControl('', Validators.required));
+  }
+
+  removeControl(control){
+    this.myForm.removeControl(control.key);
+  }
 
 }
